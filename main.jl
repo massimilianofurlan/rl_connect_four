32d4338d66154f200a99d3faf5edc7e243184445
massimilianofurlangit <frl.msm95@gmail.com>
2022-08-04 14:19:03 +0200

    update

--- main.jl
+++ main.jl
@@ -1,82 +1,63 @@
-
 using DataStructures
 using ProgressMeter
-using JLD
 
-const n_cols = 7
-const n_rows = 6
+# set board size: a regular board is 6 x 7 (too many states)
+const n_cols = 7 # regular is 7
+const n_rows = 6 # regular is 6
+const last_turn = n_cols * n_rows
 
-Q = DefaultDict(()->zeros(n_cols))
-alpha = 0.5
-delta = 0.99
+# Q-learning hyperparameters
+Q = DefaultDict(()->zeros(Float16,n_cols)) 	# the Q matrix is a DefaultDict indexed with boards
+const n_episodes = 20000000					# each episode is a self-play training match
+const alpha = Float16(0.5)					# learning rate
+const gamma = Float16(0.99)					# discount factor
+# exploration decay: e_t = beta * e_{t-1} for t \in 0,..,T where T = n_episodes and e_0 = epsilon0
+const epsilon0 = 1/2
+const beta = (0.1/epsilon0)^(1/n_episodes)	# I want e_T = 0.1
+const epsilon = [epsilon0 * beta^episode for episode in 1:n_episodes]	
 
-function isterminal(board)
-	# check rows
-	for i in 1:n_rows
-		for j in 1:n_cols-3
-			board[i,j] != 0 || continue
-			all( ==(board[i,j]), board[i,j:j+3]) && return true
+# generic functions
+
+function argmax_(A::AbstractArray; inds = Array{Int8,1}(undef,n_cols))
+	# return indices of array A's set of maxima
+	maxval = typemin(eltype(A))
+	n = 0
+	@inbounds for i in keys(A)
+		Ai = A[i]
+		Ai < maxval && continue
+		if Ai > maxval
+			maxval = Ai
+			n = 1
+			inds[n] = i
+		else
+			inds[n+=1] = i
 		end
 	end
-	# check jumn  
-	for j in 1:n_cols
-		for i in 1:n_rows-3
-			board[i,j] != 0 || continue
-			all( ==(board[i,j]), board[i:i+3,j]) && return true
-		end
-	end
-	# check upward diagonals 
-	for i in 1:n_rows-3
-		for j in 1:n_cols-3
-			board[i,j] != 0 || continue
-			all( ==(board[i,j]), [board[i+k,j+k] for k in 0:3]) && return true
-		end
-	end
-	# check donward diagonals 
-	for i in 4:n_rows
-		for j in 1:4
-			board[i,j] != 0 || continue
-			all( ==(board[i,j]), [board[i-k,j+k] for k in 0:3]) && return true
-		end
-	end
-	return false
+	return view(inds,1:n)
 end
 
-
-function get_action(Q,board,n_tokens)
-	legal_actions = (1:n_cols)[n_tokens .< 6] 
-	if rand() <= 1/3 || !haskey(Q,board)
-		return rand(legal_actions)
-	else 
-		return legal_actions[rand(findall(Q[board][n_tokens .< 6] .== maximum(Q[board][n_tokens .< 6])))]
+function get_bit_board(board)
+	# codify board using 2-bits information for each disk (0:none,1:yellow,2:red) to reduce allocations
+	bit_board = BitMatrix(undef,n_rows*2,n_cols)		# 2x1 blocks of this array are 2-bits
+	bit_board .= false									# initially set everything to 0
+	for j in 1:n_cols	
+		for i in 1:n_rows
+			if board[i,j] == Int8(1)					# if 1:yellow, then codify [1, 0]					
+				bit_board[i*2-1,j] = true 	
+				bit_board[i*2,j] = false
+			elseif board[i,j] == Int8(2)				# if 2:red, then codify [0, 1]
+				bit_board[i*2-1,j] = false	
+				bit_board[i*2,j] = true
+			end
+		end
 	end
+	return bit_board
 end
-
-
-function update_q(Q, alpha, delta, board, new_board, a; reward = 0.0)
-	if haskey(Q,new_board)
-		Q[board][a] = (1 - alpha) * Q[board][a] + alpha * (reward + delta * maximum(Q[new_board]))
-	elseif haskey(Q,board)
-		Q[board][a] = (1 - alpha) * Q[board][a] 
-	elseif reward != 0
-		Q[board][a] = (1 - alpha) * Q[board][a] + alpha * reward 
-	end
-	new_board_ = flip_board(new_board)
-	board_ = flip_board(board)
-	a_ = (n_cols:-1:1)[a]
-	if haskey(Q,new_board_)
-		Q[board_][a_] = (1 - alpha) * Q[board_][a_] + alpha * (reward + delta * maximum(Q[new_board_]))
-	elseif haskey(Q,board_)
-		Q[board_][a_] = (1 - alpha) * Q[board_][a_] 
-	elseif reward != 0
-		Q[board_][a_] = (1 - alpha) * Q[board_][a_] + alpha * reward 
-	end
-end
-
 
 function turn_board(board)
-	for i in 1:n_rows			#n_tokens[j]
-		for j in 1:n_cols
+	# 'turn' board to get next or preceding player POV
+	for j in 1:n_cols	
+		for i in 1:n_rows
 			if board[i,j] == Int8(1)
 				board[i,j] = Int8(2)
 			elseif board[i,j] == Int8(2)
@@ -88,102 +69,218 @@ function turn_board(board)
 end
 
 function flip_board(board)
-	return board[:,end:-1:1]
+	# flip board around the vertical axis
+	return view(board,:,n_cols:-1:1)
 end
 
-function self_play(Q, alpha, delta)
-	board2 = zeros(Int8, n_rows, n_cols)
-	board1 = zeros(Int8, n_rows, n_cols)
-	board = zeros(Int8, n_rows, n_cols)
-	n_tokens = zeros(Int8, n_cols)
-	turn = 1
-	winner = 0
-	a = 0
-	a1 = 0
-	# begin game
-	a1 = get_action(Q,board2,n_tokens)	
-	n_tokens[a1] += 1
-	board1[n_tokens[a1],a1] = Int8(2)	# next player sees this as the -i move
-	board[n_tokens[a1],a1] = Int8(2)	# next player sees this as the -i move
-	while turn < n_rows*n_cols
-		a = get_action(Q,board1,n_tokens)
-		n_tokens[a] += 1
-		board[n_tokens[a],a] = Int8(1)
-		#
-		isterminal(board) && break
-		# Q-update from preceding move
-		board = turn_board(board)								# preceding player perspective
-		update_q(Q, alpha, delta, board2, board, a1)	# Q update
-		#
-		a1 = copy(a)
-		board2 = copy(board1)
-		board1 = copy(board)
-		#
-		turn += 1
+function isterminal(board, turn)
+	# check if board is terminal, that is, if somebody won 
+	turn < 7 && return false			# the minimum number of turns to win is seven
+	# check rows
+	for j in 1:n_cols-3
+		for i in 1:n_rows
+			board[i,j] == 1 || continue
+			all( ==(1), view(board,i,j:j+3)) && return true
+		end
 	end
-	if turn != n_rows*n_cols
-		update_q(Q, alpha, 0.0, board1, board, a; reward = 100.0)
-		update_q(Q, alpha, delta, board2, turn_board(board), a1; reward = -100.0)
+	# check cols  
+	for j in 1:n_cols
+		for i in 1:n_rows-3
+			board[i,j] == 1 || continue
+			all( ==(1), view(board,i:i+3,j)) && return true
+		end
+	end
+	# check upward diagonals 
+	for j in 1:n_cols-3	
+		for i in 1:n_rows-3
+			board[i,j] == 1 || continue
+			all( ==(1), [board[i+k,j+k] for k in 0:3]) && return true
+		end
+	end
+	# check donward diagonals 
+	for j in 1:n_cols-3
+		for i in 4:n_rows
+			board[i,j] == 1 || continue
+			all( ==(1), [board[i-k,j+k] for k in 0:3]) && return true
+		end
+	end
+	return false
+end
+
+# Q-learning functions
+
+function get_best_actions(Q, board, n_disks)
+	# get set of best actions in current board
+	bit_board = get_bit_board(board)						# get bit board
+	legal_actions = (1:n_cols)[n_disks .< n_rows] 			# get set of feasible actions in current board
+	if haskey(Q,bit_board)
+		# if there is a known best action in the current board, bo it 
+		return legal_actions[argmax_(view(Q[bit_board],n_disks .< n_rows))]
+	end
+	flipped_bit_board = flip_board(bit_board)	
+	if haskey(Q,flipped_bit_board)
+		# if there is a known best action in the current board flipped, bo it flipped
+		return legal_actions[argmax_(view(Q[flipped_bit_board][n_cols:-1:1],n_disks .< n_rows))]
+	end
+	# if there is no best known action in the current board and current board flipped, randomize
+	return legal_actions
+end
+
+function get_action(Q, board, n_disks, epsilon)
+	# get action give board according to an epsilon greedy strategy 
+	if rand() <= epsilon
+		return rand((1:n_cols)[n_disks .< n_rows])			# explore
 	else
-		update_q(Q, alpha, 0.0, board1, board, a)					# last mover has no future
-		update_q(Q, alpha, delta, board2, turn_board(board), a1)	# 
+		return rand(get_best_actions(Q,board,n_disks))		# exploit
+	end
+end
+
+function update_q(Q, alpha, gamma, board, new_board, a, reward)
+	# check if board and new_board are known 
+	bit_board = get_bit_board(board)			# use bit_boards as indeces to reduce allocations
+	bit_new_board = get_bit_board(new_board)
+	if haskey(Q,bit_new_board)
+		# if new_board is known, full update
+		Q[bit_board][a] = (1 - alpha) * Q[bit_board][a] + alpha * (reward + gamma * maximum(Q[bit_new_board]))
+		return nothing	# update done, quit function
+	elseif haskey(Q,bit_board)
+		# if new_board is unkwnow but board is known, partial update, maximum(Q[bit_new_board] = 0
+		Q[bit_board][a] = (1 - alpha) * Q[bit_board][a] + alpha * reward
+		return nothing
+	end 
+	# if neither bit_new_board and bit_board are known, check if they are known flipped
+	flipped_bit_board = flip_board(bit_board)	
+	flipped_bit_new_board = flip_board(bit_new_board)
+	flipped_a = (n_cols:-1:1)[a]
+	if haskey(Q,flipped_bit_new_board)
+		# if flipped_new_board is known, full update
+		Q[flipped_bit_board][flipped_a] = (1 - alpha) * Q[flipped_bit_board][flipped_a] + alpha * (reward + gamma * maximum(Q[flipped_bit_new_board]))
+		return nothing
+	elseif haskey(Q,flipped_bit_board)
+		# if flipped_new_board is unkwnow but flipped_board is known, partial update,  maximum(Q[flipped_bit_new_board] = 0
+		Q[flipped_bit_board][flipped_a] = (1 - alpha) * Q[flipped_bit_board][flipped_a]  + alpha * reward
+		return nothing
+	end
+	if reward != 0
+		# if board and new_board are unknown, both flipped and not as is, and reward != 0, partial update: Q[bit_board][a] = 0, maximum(Q[bit_new_board] = 0
+		# everything starts here: first time winning/losing this way 
+		Q[bit_board][a] = alpha * reward 
+	end
+end
+
+# main functions
+
+function self_play(Q, alpha, gamma, epsilon)
+	# training self-play environment
+	pre_board = zeros(Int8, n_rows, n_cols)		# board before current player move 
+	board = zeros(Int8, n_rows, n_cols)			# current board
+	post_board = zeros(Int8, n_rows, n_cols) 	# board after current player move
+	n_disks = zeros(Int8, n_cols)				# number of disks per column
+	a, pre_a = undef, undef						# initialize curerent action and preceding player action
+	turn = 1 									# turn number
+	for turn in 1:last_turn
+		a = get_action(Q, board, n_disks, epsilon)					# get e-greedy action given current board
+		n_disks[a] += 1 											# store disk
+		post_board[n_disks[a],a] = Int8(1)							# update post_board (agent always plays 1:yellow)
+		if isterminal(post_board, turn)								# check if current player won
+			# current player gets a 100 reward
+			update_q(Q, alpha, 0.0, board, post_board, a, Float16(100.0))
+			# preceding player gets a -100 reward
+			post_board = turn_board(post_board)						# preceding player POV
+			update_q(Q, alpha, gamma, pre_board, post_board, pre_a, Float16(-100.0))
+			# end of game
+			break
+		end 
+		# non-winning move gives 0 reward: note, sequential moves (s_i, a_i) -> (s_j, a_j) -> (s'_i, ...)
+		post_board = turn_board(post_board)							# preceding player POV
+		turn != 1 && update_q(Q, alpha, gamma, pre_board, post_board, pre_a, Float16(0.0))
+		# transition game to next player environment
+		pre_a = copy(a)
+		pre_board = copy(board)
+		board = copy(post_board)
 	end
 	return Q
 end
 
-n_sessions = 10000000
-progress = Progress(n_sessions, color=:white, showspeed=true)
-@time for i in 1:n_sessions
-	global Q = self_play(Q, alpha, delta)
-	next!(progress)
-end
-#save(Q,"Q.jld")
+# begin execution 
 
-
-for key in keys(Q)
-	display("text/plain", key[end:-1:1,:])
+println("Wait, I am training ... (around 15 minutes, Apple Silicon M1 Pro)")
+# training sessions
+progress = Progress(n_episodes, color=:white, showspeed=true)
+@time for episode in 1:n_episodes
+	# self-play training, epsilon decays according to e_t = beta * e_{t-1} each episode
+	global Q = self_play(Q, alpha, gamma, epsilon[episode])
+	next!(progress,; showvalues = [(:episode,episode),(:epsilon,epsilon[episode]),(:known_boards, length(Q))])
 end
 
+print("Training finished, press enter to continue ... ")
+readline()
 
-function game_human_cpu(Q)
+function get_stats(board, Q, n_disks)
+	# print some nice stats 
+	bit_board = get_bit_board(board)
+	flipped_bit_board = flip_board(bit_board)
+	if haskey(Q,bit_board)
+		println("\n\nThe agent knows this board ", Q[bit_board])
+		println("The known best action is ", get_best_actions(Q,board,n_disks))
+		println("The continuation value is ", unique(Q[bit_board][get_best_actions(Q,board,n_disks)]))
+	elseif haskey(Q,flipped_bit_board)
+		println("\n\nThe agent knows this board: ", Q[flipped_bit_board][end:-1:1])
+		println("The known best action is ", get_best_actions(Q,board,n_disks))
+		println("The continuation value is ", unique(Q[flipped_bit_board][get_best_actions(Q,board,n_disks)]))
+	else	
+		println("\n\nThis board is unknown")
+	end
+end
+
+function play_against_agent(Q; players = ["agent","human"])
+	# ugly function to play against the agent: one might want to use gtk...
 	board = zeros(Int8, n_rows, n_cols)
-	n_tokens = zeros(Int8, n_cols)
-	turn = 1
-	winner = 0
-	a1 = 0
-	a2 = 0
-
-	while !isterminal(board)
-		display("text/plain", board[end:-1:1,:])
-		if isodd(turn)
+	n_disks = zeros(Int8, n_cols)
+	for turn in 1:last_turn
+		if players[1] == "agent"
 			run(`clear`)
-			if haskey(Q,board)
-				println("This board is known: ", round.(Q[board],digits = 4))
-			else 
-				println("This board is unknown")
-			end
-			while true
-				a1 = rand(findall(Q[board] .== maximum(Q[board][n_tokens .< 6])))
-				n_tokens[a1] < 6 && break
-			end
-			n_tokens[a1] += 1
-			board[n_tokens[a1],a1] = Int8(1)
-			
+			println("Agent turn ...\n")
+			display("text/plain", board[end:-1:1,:])
+			get_stats(board, Q, n_disks)
+			a = rand(get_best_actions(Q, board, n_disks))
+			println("Agent took action $a \n")
+			n_disks[a] += 1
+			board[n_disks[a],a] = Int8(1)
+			isterminal(board, turn) && break
 		else 
+			println("Human turn ...\n")
+			display("text/plain", board[end:-1:1,:])
 			while true
-				print("\n1 player select row: ")
-				a2 = parse(Int,readline())
-				n_tokens[a2] < 6 && break
+				print("\n\nHuman, select row: ")
+				a = parse(Int,readline())
+				a in 1:n_cols && n_disks[a] < n_rows && break
 			end
-			n_tokens[a2] += 1
-			board[n_tokens[a2],a2] = Int8(2)
+			n_disks[a] += 1
+			board[n_disks[a],a] = Int8(2)
+			human_won = isterminal(turn_board(board), turn) 
+			turn_board(board)
+			human_won && break
 		end
-		turn += 1
+		players = players[end:-1:1]
 	end 
-	println("\nPlayer $winner wins")
+	println()
+	display("text/plain", board[end:-1:1,:])
+	println("\n\n ### The ",players[1]," won ### \n")
 end
 
-@profilehtml for i in 1:100
-	global Q = self_play(Q, alpha, delta)
+players = ["agent","human"]					# first match, agent moves first
+while true
+	global players
+	play_against_agent(Q; players = players)
+	print("Want to play again? [y,n]: ")	# default is y
+	readline() != "n" || break				# exit game if 'n', play again otherwise
+	players = players[end:-1:1]				# alternating first mover
 end
+
+
+
+
+
+
 
